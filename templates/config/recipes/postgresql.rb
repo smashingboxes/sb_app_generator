fd0affc24c050a4c4dae0c02c504decd42146fa4
Josh Barber <roar@jorshasaur.com>
2013-04-12 15:06:57 -0300

    Changing postgres user to deploy user
    Hyphens cause chaos in psql, so removing the possible occurrence should remove headaches.

--- templates/config/recipes/postgresql.rb
+++ templates/config/recipes/postgresql.rb
@@ -1,5 +1,5 @@
 set_default(:postgresql_host, "localhost")
-set_default(:postgresql_user) { application }
+set_default(:postgresql_user) { user }
 set_default(:postgresql_password) { Capistrano::CLI.password_prompt "PostgreSQL Password: " }
 set_default(:postgresql_database) { "#{application}_production" }
 
