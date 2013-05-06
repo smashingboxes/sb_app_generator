ee9e3dd99b70f4c5427f2578588797d6f0e4a201
Michael Elfassy <michaelelfassy@gmail.com>
2013-05-06 10:26:13 -0300

    fix for bug with asset precompilation
    see https://github.com/capistrano/capistrano/issues/224

--- templates/config/recipes/base.rb
+++ templates/config/recipes/base.rb
@@ -20,8 +20,12 @@ namespace :deploy do
 
   namespace :assets do
     task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+      begin
+        from = source.next_revision(current_revision) # <-- Fail here at first-time deploy
+      rescue
+        err_no = true
+      end
+      if err_no || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
       else
         logger.info "Skipping asset pre-compilation because there were no asset changes"
