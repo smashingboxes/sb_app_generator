e22120872afa39331b12ba2acecb9365cb3e4cf8
Michael Elfassy <michael@smashingboxes.com>
2013-05-06 09:51:49 -0400

    commented out custom asset precompile
    should work well now only with the sproket gem

---
+++ templates/config/recipes/assets.rb
@@ -0,0 +1,19 @@
+# skip assets precompile if no assets were changed
+# not needed if you use the sprockets gem (or https://github.com/ndbroadbent/turbo-sprockets-rails3)
+
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      begin
+        from = source.next_revision(current_revision) # <-- Fail here at first-time deploy
+      rescue
+        deploy_cold = true
+      end
+      if deploy_cold || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+end
--- templates/config/recipes/base.rb
+++ templates/config/recipes/base.rb
@@ -17,15 +17,4 @@ namespace :deploy do
     run "#{sudo} apt-get -y update"
     run "#{sudo} apt-get -y install python-software-properties software-properties-common libxslt-dev libxml2-dev"
   end
-
-  namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
-  end
 end
