3503fd35e50016712bcb077716a102a892c0dd89
Michael Elfassy <michaelelfassy@gmail.com>
2013-05-22 16:50:14 -0300

    bug in paperclip file

--- templates/config/recipes/paperclip.rb
+++ templates/config/recipes/paperclip.rb
@@ -8,10 +8,8 @@ namespace :paperclip do
 
 
   desc "build missing paperclip styles"
-    task :build_missing_paperclip_styles, :roles => :app do
-      run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake paperclip:refresh:missing_styles"
-    end
+  task :build_missing_paperclip_styles, :roles => :app do
+    run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake paperclip:refresh:missing_styles"
   end
+  after "deploy:update_code", "paperclip:build_missing_paperclip_styles"
 end
-
-after("deploy:update_code", "paperclip:build_missing_paperclip_styles")
