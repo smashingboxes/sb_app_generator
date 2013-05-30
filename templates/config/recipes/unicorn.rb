596810ec5b56b47878171aefe8a68709f25df0d3
Michael Elfassy <michael@smashingboxes.com>
2013-05-30 13:23:35 -0400

    Unicorn use upgrade instead of restart
    should solve some asset precompile issues

--- templates/config/recipes/unicorn.rb
+++ templates/config/recipes/unicorn.rb
@@ -1,5 +1,6 @@
 set_default(:unicorn_user) { user }
 set_default(:unicorn_pid) { "#{current_path}/tmp/pids/unicorn.pid" }
+set_default(:unicorn_sock) { "/tmp/unicorn.#{application}.sock" }
 set_default(:unicorn_config) { "#{shared_path}/config/unicorn.rb" }
 set_default(:unicorn_log) { "#{shared_path}/log/unicorn.log" }
 set_default(:unicorn_workers, 2)
@@ -16,31 +17,22 @@ namespace :unicorn do
   end
   after "deploy:setup", "unicorn:setup"
 
-  %w[start stop restart].each do |command|
+  %w[start stop restart upgrade].each do |command|
     desc "#{command} unicorn"
     task command, roles: :app do
       run "service unicorn_#{application} #{command}"
       run "tail -n 20 #{unicorn_log}"
     end
-    after "deploy:#{command}", "unicorn:#{command}"
+    # after "deploy:#{command}", "unicorn:#{command}"
+  end
+  after "deploy:start", "unicorn:#start"
+  after "deploy:stop", "unicorn:#stop"
+  after "deploy:restart", "unicorn:upgrade"
+
+  desc "Force hard Unicorn restart"
+  task :force_reboot, roles: :app do
+    run "service unicorn_#{application} stop"
+    run "service unicorn_#{application} start"
   end
 
-  # before "unicorn:restart", "deploy:assets:precompile"
-  # before "unicorn:start", "deploy:assets:precompile"
 end
-
-
-# desc "Zero-downtime restart of Unicorn"
-# task :restart, :except => { :no_release => true } do
-#   run "kill -s USR2 `cat #{shared_path}/pids/unicorn.pid`"
-# end
-
-# desc "Start unicorn"
-# task :start, :except => { :no_release => true } do
-#   run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D -E production"
-# end
-
-# desc "Stop unicorn"
-# task :stop, :except => { :no_release => true } do
-#   run "kill -s QUIT `cat #{shared_path}/pids/unicorn.pid`"
-# end
