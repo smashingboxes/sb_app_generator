35738fd79eaaf24f8dd1016c3ed56959126d2f54
Michael Elfassy <michaelelfassy@gmail.com>
2013-08-07 10:20:31 -0400

    Update test_helper.rb

--- templates/test/test_helper.rb
+++ templates/test/test_helper.rb
@@ -3,47 +3,51 @@ SimpleCov.start 'rails'
 
 ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
-# Require support files
-Dir[File.expand_path('../support/*.rb', __FILE__)].each {|file| require file}
- 
+require 'rails/test_help' 
 require 'factory_girl'
 require 'minitest/autorun'
 require 'minitest/rails'
 require 'minitest/rails/capybara'
- 
-begin; require 'turn/autorun'; rescue LoadError; end
-DatabaseCleaner.strategy = :transaction 
 
+# Support files
+Dir[File.expand_path('../support/*.rb', __FILE__)].each {|file| require file}
+# Database cleaner
+DatabaseCleaner.strategy = :transaction 
+# Coloring
+begin; require 'turn/autorun'; rescue LoadError; end
+
+# All tests
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
-  
-  # Add more helper methods to be used by all tests here...
   include FactoryGirl::Syntax::Methods
-  
-  
+
   def self.prepare
    DatabaseCleaner.clean
     # Add code that needs to be executed before test suite start
   end
   prepare
+end
+ 
+ # Integration Tests
+class ActionDispatch::IntegrationTest < ActiveSupport::TestCase
+  include Rails.application.routes.url_helpers
+  include Capybara::DSL
+  include Warden::Test::Helpers
+  Warden.test_mode!
 
   def setup
    # Add code that need to be executed before each test
   end
-
   def teardown
-   # Add code that need to be executed after each test
+    Warden.test_reset! 
   end
 end
- 
-class ActionDispatch::IntegrationTest < ActiveSupport::TestCase
-  include Rails.application.routes.url_helpers
-  include Capybara::DSL
+
+# Controller Test
+class ActionController::TestCase < ActiveSupport::TestCase
+  include Devise::TestHelpers
 end
 
-# ActiveSupport::TestCase
-# ActionController::TestCase
 # ActionView::TestCase
 # ActionMailer::TestCase
 # ActionDispatch::TestCase 
