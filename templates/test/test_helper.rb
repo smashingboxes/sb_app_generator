7f8d8a245cbec284c51cce7f7ea2c39f6d9dadde
Michael Elfassy <michaelelfassy@gmail.com>
2013-08-06 22:12:02 -0400

    Update test_helper.rb

--- templates/test/test_helper.rb
+++ templates/test/test_helper.rb
@@ -13,16 +13,33 @@ require 'minitest/rails'
 require 'minitest/rails/capybara'
  
 begin; require 'turn/autorun'; rescue LoadError; end
+DatabaseCleaner.strategy = :transaction 
 
 class ActiveSupport::TestCase
+  ActiveRecord::Migration.check_pending!
+  
+  # Add more helper methods to be used by all tests here...
   include FactoryGirl::Syntax::Methods
+  
+  
+  def self.prepare
+   DatabaseCleaner.clean
+    # Add code that needs to be executed before test suite start
+  end
+  prepare
+
+  def setup
+   # Add code that need to be executed before each test
+  end
+
+  def teardown
+   # Add code that need to be executed after each test
+  end
 end
  
 class ActionDispatch::IntegrationTest < ActiveSupport::TestCase
   include Rails.application.routes.url_helpers
   include Capybara::DSL
-  
-  # include BoostrapMacros
 end
 
 # ActiveSupport::TestCase
