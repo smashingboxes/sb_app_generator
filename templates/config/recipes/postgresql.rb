e09ac2192f30be5a64b1a122361d0e76a8726904
Michael Elfassy <michaelelfassy@gmail.com>
2013-08-07 15:47:57 -0400

    make postgres recipe omnipotent

--- templates/config/recipes/postgresql.rb
+++ templates/config/recipes/postgresql.rb
@@ -1,22 +1,35 @@
-set_default(:postgresql_host, "localhost")
-set_default(:postgresql_user) { user }
-set_default(:postgresql_password) { Capistrano::CLI.password_prompt "PostgreSQL Password: " }
-set_default(:postgresql_database) { "#{application.downcase}_production" }
+set(:postgresql_host) { 'localhost' }
+set(:postgresql_user) { user }
+set(:postgresql_password) { Capistrano::CLI.password_prompt 'PostgreSQL Password: ' }
+set(:postgresql_database) { "#{application}_#{rails_env}" }
 
 namespace :postgresql do
+
   desc "Install the latest stable release of PostgreSQL."
   task :install, roles: :db, only: {primary: true} do
     run "#{sudo} add-apt-repository -y ppa:pitti/postgresql"
     run "#{sudo} apt-get -y update"
     run "#{sudo} apt-get -y install postgresql libpq-dev"
   end
-  after "deploy:install", "postgresql:install"
-
-  desc "Create a database for this application."
-  task :create_database, roles: :db, only: {primary: true} do
-    run %Q{#{sudo} -u postgres psql -c "create user #{postgresql_user} with password '#{postgresql_password}';"}
-    run %Q{#{sudo} -u postgres psql -c "create database #{postgresql_database} owner #{postgresql_user};"}
+  
+  desc 'Create a database for the application'
+  task :create_database, :roles => :db, only: { primary: true } do
+    create_user postgresql_user, postgresql_password
+    create_database postgresql_database, postgresql_user
   end
-  after "deploy:setup", "postgresql:create_database"
 
+  # Utilities
+  def create_database(database, username)
+    sudo %Q(psql -c 'create database "#{database}" owner "#{username}"'), as: 'postgres'
+  rescue
+    logger.important "PostreSQL database already exists"
+  end
+
+  def create_user(username, password)
+    sudo %Q(psql -c "create user \\"#{username}\\" with password '#{password}'"), as: 'postgres'
+  rescue
+    logger.important "PostreSQL user already exists"
+  end
 end
+after "deploy:install",         "postgresql:install"
+after 'deploy:setup',           'postgresql:create_database'
