22be5f4bfbda318624b4d419ea7d072441969bc1
Michael Elfassy <michaelelfassy@gmail.com>
2013-09-25 09:37:57 -0400

    better defaults for production

--- templates/config/deploy/production.rb
+++ templates/config/deploy/production.rb
@@ -1,4 +1,3 @@
-
 role :web, "{ip}"
 role :app, "{ip1}", "{ip2}"
 role :queue, "{ip}"
@@ -6,3 +5,6 @@ role :memcache, "{ip}"
 role :db, "{ip}", primary: true
 
 set :rails_env, 'production' 
+
+set :memcached_memory_limit, 256 #MB
+set :unicorn_workers, 3 #usually number of cpus (per app server) + 1
