f07a3e8443131b2c99d16d67e4426ae1d025b5b7
Michael Elfassy <michaelelfassy@gmail.com>
2013-10-17 08:53:40 -0400

    ask for tag when deploying to production
    best practice is to use tags to refer to stable releases

--- templates/config/deploy/production.rb
+++ templates/config/deploy/production.rb
@@ -8,3 +8,10 @@ set :rails_env, 'production'
 
 set :memcached_memory_limit, 256 #MB
 set :unicorn_workers, 3 #usually number of cpus (per app server) + 1
+
+set :branch do
+  default_tag = `git tag`.split("\n").last
+  tag = Capistrano::CLI.ui.ask "Tag to deploy (make sure to push the tag first): [#{default_tag}] "
+  tag = default_tag if tag.empty?
+  tag
+end
