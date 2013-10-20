18bb0c533072458e506273032827d87eac2f2856
Michael Elfassy <michael@smashingboxes.com>
2013-10-20 15:41:39 -0400

    missing defaults for javascript

--- templates/app/assets/javascripts/application.js
+++ templates/app/assets/javascripts/application.js
@@ -1,3 +1,6 @@
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
 //= require 'foundation/modernizr.foundation'
 //= require 'foundation/jquery.reveal'
 //= require 'foundation/jquery.orbit-1.4.0'
@@ -5,3 +8,9 @@
 //= require 'foundation/jquery.placeholder.min'
 //= require 'foundation/jquery.tooltips'
 //= require 'foundation/app'
+
+$(document).on('ready page:load', function(){
+
+  $(document).foundation(); 
+
+})
