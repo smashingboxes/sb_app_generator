3de38c7157f22118ca2e6bed3d2fea68658c695a
Leonel Galan <leonelgalan@gmail.com>
2013-12-03 18:11:10 -0500

    Renames old foundation names to newer names, requires less files

--- templates/app/assets/javascripts/application.js
+++ templates/app/assets/javascripts/application.js
@@ -1,16 +1,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require 'foundation/modernizr.foundation'
-//= require 'foundation/jquery.reveal'
-//= require 'foundation/jquery.orbit-1.4.0'
-//= require 'foundation/jquery.customforms'
-//= require 'foundation/jquery.placeholder.min'
-//= require 'foundation/jquery.tooltips'
-//= require 'foundation/app'
+//= require vendor/custom.modernizr
+//= require foundation/foundation
 
 $(document).on('ready page:load', function(){
-
-  $(document).foundation(); 
-
+  $(document).foundation();
 });
