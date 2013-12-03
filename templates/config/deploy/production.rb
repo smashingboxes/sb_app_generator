60ed104c27cc2ad8851147ae214025d23e439f47
Leonel Galan <leonelgalan@gmail.com>
2013-12-03 18:11:28 -0500

    Simplifies production settings to single server

--- templates/config/deploy/production.rb
+++ templates/config/deploy/production.rb
@@ -1,10 +1,5 @@
-role :web, "{ip}"
-role :app, "{ip1}", "{ip2}"
-role :queue, "{ip}"
-role :memcache, "{ip}"
-role :db, "{ip}", primary: true
-
-set :rails_env, 'production' 
+server '{{server_ip}}', :web, :app, :queue, :memcache, :db, primary: true
+set :rails_env, 'production'
 
 set :memcached_memory_limit, 256 #MB
 set :unicorn_workers, 3 #usually number of cpus (per app server) + 1
