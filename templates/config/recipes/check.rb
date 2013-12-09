112a285da7eb1637c4a84d5e04a2a76f44fbf016
Michael Elfassy <michaelelfassy@gmail.com>
2013-12-09 10:58:08 -0500

    skip check if deploying a tag

--- templates/config/recipes/check.rb
+++ templates/config/recipes/check.rb
@@ -1,8 +1,8 @@
 namespace :check do
   desc "Make sure local git is in sync with remote."
   task :revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse #{branch}` #`git rev-parse origin/#{branch}`
-      puts "WARNING: HEAD is not the same as origin/#{branch}"
+   if `git rev-parse HEAD` != `git rev-parse #{branch}` && !(`git tag`.split("\n").include?(branch))
+      puts "WARNING: HEAD is not the same as #{branch}"
       puts "Run `git push` to sync changes."
       exit
     end
