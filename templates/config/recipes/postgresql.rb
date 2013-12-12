6eb15fed7546d2863d14979c3c80bc3e05368cc3
Leonel Galan <leonelgalan@gmail.com>
2013-12-12 15:10:56 -0500

    Upgrades postgresql.rb
    By upgrades, I mean:
    - separates create_user and create_database
    - ppa:pitti/postgresql is deprecated, uses preffered method instead:
    http://wiki.postgresql.org/wiki/Apt

--- templates/config/recipes/postgresql.rb
+++ templates/config/recipes/postgresql.rb
@@ -1,35 +1,41 @@
-set_default(:postgresql_host) { 'localhost' }
+set_default :postgresql_host, 'localhost'
 set_default(:postgresql_user) { user }
 set_default(:postgresql_password) { Capistrano::CLI.password_prompt 'PostgreSQL Password: ' }
 set_default(:postgresql_database) { "#{application}_#{rails_env}" }
 
 namespace :postgresql do
-
-  desc "Install the latest stable release of PostgreSQL."
+  desc 'Install the latest stable release of PostgreSQL.'
   task :install, roles: :db, only: {primary: true} do
-    run "#{sudo} add-apt-repository -y ppa:pitti/postgresql"
-    run "#{sudo} apt-get -y update"
-    run "#{sudo} apt-get -y install postgresql libpq-dev"
+    "#{sudo} echo 'deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+    "#{sudo} wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -"
+    "#{sudo} apt-get -y update"
+    "#{sudo} apt-get -y install postgresql-9.3 libpq-dev"
   end
-  
-  desc 'Create a database for the application'
-  task :create_database, :roles => :db, only: { primary: true } do
+
+  desc 'Create a database user for the application'
+  task :create_user, roles: :db, only: {primary: true} do
     create_user postgresql_user, postgresql_password
+  end
+
+  desc 'Create a database for the application'
+  task :create_database, roles: :db, only: {primary: true} do
     create_database postgresql_database, postgresql_user
   end
 
   # Utilities
   def create_database(database, username)
-    sudo %Q(psql -c 'create database "#{database}" owner "#{username}"'), as: 'postgres'
+    sudo %{psql -c 'create database "#{database}" owner "#{username}"'}, as: 'postgres'
   rescue
-    logger.important "PostreSQL database already exists"
+    logger.important 'PostreSQL database already exists'
   end
 
   def create_user(username, password)
-    sudo %Q(psql -c "create user \\"#{username}\\" with password '#{password}'"), as: 'postgres'
+    sudo %{psql -c 'create user "#{username}" with password "#{password}"'}, as: 'postgres'
   rescue
-    logger.important "PostreSQL user already exists"
+    logger.important 'PostreSQL user already exists'
   end
 end
-after "deploy:install",         "postgresql:install"
-after 'deploy:setup',           'postgresql:create_database'
+
+after 'deploy:install', 'postgresql:install'
+after 'deploy:setup', 'postgresql:create_user'
+after 'postgresql:create_user', 'postgresql:create_database'
